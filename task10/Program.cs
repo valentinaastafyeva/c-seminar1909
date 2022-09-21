dfc18f0b0265fc1f1b99b0c95e432f9195f70f43
valentina <astafjewa.valentina@gmail.com>
2022-09-22 00:49:54 +0300

    second digit

---
+++ task10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Enter a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = num/100;
+int b = num%100;
+int c = b/10;
+Console.WriteLine($"{c} is the second digit");
